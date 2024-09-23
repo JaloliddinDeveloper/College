dde17bed84bc97f0ac837c86312612a31e24284f
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2024-09-23 23:11:11 +0500

    BROKERS: Student CRUD

--- College.Web/Brokers/Storages/StorageBroker.Student.cs
+++ College.Web/Brokers/Storages/StorageBroker.Student.cs
@@ -10,5 +10,20 @@ namespace College.Web.Brokers.Storages
     public partial class StorageBroker
     {
         public DbSet<Student> Students { get; set; }
+
+        public async ValueTask<Student> InsertStudentAsync(Student student)=>
+            await InsertAsync(student);
+
+        public IQueryable<Student> SelectAllStudents() =>
+            SelectAll<Student>();
+       
+        public async ValueTask<Student> SelectStudentByIdAsync(int studentId)=>
+            await SelectAsync<Student>(studentId);
+       
+        public async ValueTask<Student> UpdateStudentAsync(Student student)=>
+            await UpdateAsync(student);
+       
+        public async ValueTask<Student> DeleteStudentAsync(Student student)=>
+            await DeleteAsync(student);
     }
 }
