84c3c67ec9a25fbca692c8fcd00270238621d3e5
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2024-09-27 23:58:04 +0500

    Make Index Beautifel

---
+++ College.Web/Attributes/AllowedExtensionsAttribute.cs
@@ -0,0 +1,33 @@
+﻿//--------------------------------------------------
+// Copyright (c) Coalition Of Good-Hearted Engineers
+// Free To Use To Find Comfort And Peace
+//--------------------------------------------------
+using System.ComponentModel.DataAnnotations;
+
+namespace College.Web.Attributes
+{
+    public class AllowedExtensionsAttribute : ValidationAttribute
+    {
+        private readonly string[] extensions;
+        public AllowedExtensionsAttribute(string[] extensions)=>
+            this.extensions = extensions;
+        
+
+        protected override ValidationResult IsValid(
+        object value, ValidationContext validationContext)
+        {
+            var file = value as IFormFile;
+            if (file != null)
+            {
+                var extension = Path.GetExtension(file.FileName);
+                if (!this.extensions.Contains(extension.ToLower()))
+                {
+                    return new ValidationResult($"Only files with a valid extensions ({string.Join(", ", this.extensions)}) are allowed.");
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
+
