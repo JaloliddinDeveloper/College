9209588be1ced4a7797338c3195bcc12cb8b3952
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2024-09-30 23:53:12 +0500

    BROKERS: LoggingBroker Implement

---
+++ College.Web/Brokers/Loggings/ILoggingBroker.cs
@@ -0,0 +1,12 @@
+﻿//--------------------------------------------------
+// Copyright (c) Coalition Of Good-Hearted Engineers
+// Free To Use To Find Comfort And Peace
+//--------------------------------------------------
+namespace College.Web.Brokers.Loggings
+{
+    public interface ILoggingBroker
+    {
+        void LogError(Exception exception);
+        void LogCritical(Exception exception);
+    }
+}
